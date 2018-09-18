ea71080dbe1608b0e8c4061fd182542f131445c0
cunmin <450047998@qq.com>
2018-09-18 14:49:17 +0800

    输入栏位置问题

--- keyboardhelper/src/main/java/com/littleyellow/keyboardhelper/RegisterHelper.java
+++ keyboardhelper/src/main/java/com/littleyellow/keyboardhelper/RegisterHelper.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.graphics.Rect;
 import android.os.Build;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.View;
 
 /**
